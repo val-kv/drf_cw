78bc3ab0c395e6d1853754fa2667f66d8e8789c7
Kvashin Valeriy <val_kv@mail.ru>
2024-07-11 01:12:36 +0200

    edit settings and swagger

--- drf_cw/settings.py
+++ drf_cw/settings.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = dict(DEFAULT_PERMISSION_CLASSES=[
     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
 ], DEFAULT_AUTHENTICATION_CLASSES=(
-    'rest_framework_simplejwt.authentication.JWTAuthentication'))
+    'rest_framework_simplejwt.authentication.JWTAuthentication',))
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
@@ -167,7 +167,7 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 CELERY_BEAT_SCHEDULE = {
     'periodic_task_name': {
-        'task': 'your_app.tasks.periodic_task_function',
+        'task': 'habits.tasks.periodic_task_function',
         'schedule': 3600,  # каждый час
     },
 }
--- habits/swagger.py
+++ habits/swagger.py
@@ -2,9 +2,9 @@ from drf_yasg.inspectors import SwaggerAutoSchema
 
 
 class CustomAutoSchema(SwaggerAutoSchema):
-    def get_operation(self, path, method):
+    def get_operation(self, operation_keys=None):
         # Получаем информацию о методе и пути запроса
-        operation = super().get_operation(path, method)
+        operation = super().get_operation(operation_keys=None)
 
         # Проверяем, что пользователь авторизован
         if not getattr(operation, 'permissions', {}).get('get', {}).get('is_authenticated', True):
